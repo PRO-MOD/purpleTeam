3b52810044224e0c7dfecdce52c1364dca94e5f0
chinmay0910 <chinmayrmhatre@gmail.com>
2024-05-09 03:45:00 +0530

    issues on displaying user specific reports3 notification issue

--- Frontend/src/components/HomePage.jsx
+++ Frontend/src/components/HomePage.jsx
@@ -61,8 +61,8 @@ function HomePage() {
               <Route exact path='/UserHome' element={<UserHomePage />} />
               <Route exact path='/UserHome/report/SITREP' element={<Report />} />
               <Route exact path='/UserHome/report/incident' element={<IncidentReport />} />
-              <Route exact path='/UserHome/report/day-end' element={<Notification />} />
-              <Route exact path='/UserHome/report/day-end' element={<Notification />} />
+              {/* <Route exact path='/UserHome/report/day-end' element={<Notification />} /> */}
+              <Route exact path='/UserHome/report/notification' element={<Notification />} />
               <Route exact path="/user/:userId" element={<UserDetails/>} />
               <Route exact path="/notes" element={<Home showAlert={showAlert}/>}/>
               <Route exact path="/chat/*" element={<ChatMainPage/>}/>
