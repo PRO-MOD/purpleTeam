4fc87b365fa74683fc5eee5dc73a94750520f658
chinmay0910 <chinmayrmhatre@gmail.com>
2024-05-18 16:55:40 +0530

    Improved Logic of Challenge Submissions

--- Frontend/src/components/ChallengSubmissions.jsx
+++ Frontend/src/components/ChallengSubmissions.jsx
@@ -20,7 +20,7 @@ function ChallengeSubmissions() {
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold mb-8">Challenge Submissions</h1><hr />
+            <h1 className="text-3xl font-bold mb-8">Machine Captures: </h1><hr />
             <div className="w-[90%] ">
                 {
                     submissions.length > 0 ?
