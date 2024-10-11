13843582c77edd6ea738c5da8eba834caa26b7f7
pramod garhwal <pramodgarhwaljii@gmail.com>
2024-10-12 02:23:13 +0530

    table color

--- Frontend/src/context/ColorState.jsx
+++ Frontend/src/context/ColorState.jsx
@@ -7,10 +7,11 @@ const ColorState = (props) => {
     const [bgColor, setBgColor] = useState("#e1defc");       // Default background color
     const [textColor, setTextColor] = useState("#000000");   // Default text color
     const [sidenavColor, setSidenavColor] = useState("#00174d"); // Default sidenav color
-    const [hoverColor, setHoverColor] = useState("#3652f3"); // Default hover color
+    const [hoverColor, setHoverColor] = useState(" #3652f3"); // Default hover color
+    const [tableColor, setTableColor]=useState("#bc8fee");
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-
+   
     // Function to update background color
     const updateBgColor = (color) => {
         setLoading(true);
@@ -58,10 +59,20 @@ const ColorState = (props) => {
             setLoading(false);
         }
     };
+    const updateTableColor = (color) => {
+        setLoading(true);
+        try {
+            setTableColor(color);
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <ColorContext.Provider value={{ 
-            bgColor, textColor, sidenavColor, hoverColor
+            bgColor, textColor, sidenavColor, hoverColor, tableColor
         }}>
             {props.children}
         </ColorContext.Provider>
