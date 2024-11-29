76325032a0a75b65e9bde503849a946bcacaf601
chinmay0910 <chinmayrmhatre@gmail.com>
2024-11-29 18:49:05 +0530

    tried solving vercel bug

--- Frontend/src/components/UserScorePage.jsx
+++ Frontend/src/components/UserScorePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import ScoreTable from './ScoreTable';
-import UserScoreTable from './Userscoretable';
+import UserScoreTable from './UserScoreTable';
 import { useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckDouble, faDownload, faList } from '@fortawesome/free-solid-svg-icons';
