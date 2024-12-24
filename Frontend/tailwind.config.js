e71906cc0dcd0e5cb6f396880906b09ebc5b3ed7
pramod garhwal <pramodgarhwaljii@gmail.com>
2024-12-24 12:55:58 +0530

    taiwind installed

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,40 +1,48 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
+// // /** @type {import('tailwindcss').Config} */
+// // export default {
+// //   content: [
+// //     "./index.html",
+// //     "./src/**/*.{js,ts,jsx,tsx}",
+// //   ],
+// //   theme: {
+// //     extend: {},
+// //   },
+// //   plugins: [],
+// // }
+// // tailwind.config.js
+
+// module.exports = {
 //   content: [
 //     "./index.html",
 //     "./src/**/*.{js,ts,jsx,tsx}",
 //   ],
 //   theme: {
-//     extend: {},
+//     extend: {
+//       colors: {
+//         brown: {
+//           100: '#EEE2DC',
+//           200: '#D3BABA',
+//           300: '#B99595',
+//           400: '#A67F7F',
+//           500: '#8B4513',
+//           600: '#753A11',
+//           700: '#5E2F0E',
+//           800: '#48250B',
+//           900: '#311A08',
+//           550: '#212529',
+//           450:'#B7410E',
+//           650:'#3C0008',
+//         },
+//       },
+//     },
 //   },
 //   plugins: [],
 // }
-// tailwind.config.js
 
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        brown: {
-          100: '#EEE2DC',
-          200: '#D3BABA',
-          300: '#B99595',
-          400: '#A67F7F',
-          500: '#8B4513',
-          600: '#753A11',
-          700: '#5E2F0E',
-          800: '#48250B',
-          900: '#311A08',
-          550: '#212529',
-          450:'#B7410E',
-          650:'#3C0008',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
